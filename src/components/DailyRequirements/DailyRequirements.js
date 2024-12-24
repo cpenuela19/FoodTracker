0eb7006727887e61877a874df362aa024ba019bd
cpenuela19 <ce.penuela20@uniandes.edu.co>
2024-12-23 20:44:02 -0500

    v1

--- src/components/DailyRequirements/DailyRequirements.js
+++ src/components/DailyRequirements/DailyRequirements.js
@@ -1,10 +1,20 @@
-
 import './DailyRequirements.css';
 
 function DailyRequirements() {
     return (
         <div className="daily-requirements">
             <h2>Daily Requirements</h2>
+            <p>Let's breakdown your daily food requirements</p>
+            <div className="daily-requirements-form">
+                    <div>
+                        <label htmlFor="age">Age</label>
+                        <input type="number" id="age" name="age" />
+                    </div>
+                    <div>
+                        <label htmlFor="weight">Weight</label>
+                        <input type="number" id="weight" name="weight" />
+                    </div>
+            </div>
         </div>
     );
 }
